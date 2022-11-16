63270acd94517967d0d4073d60fc8f94f9a4836b
BillyHePro <72700305+BillyHePro@users.noreply.github.com>
2022-11-16 18:14:23 +0800

    Update mainloop.py

--- mainloop.py
+++ mainloop.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# －－－－湖南创乐博智能科技有限公司－－－－
-#  文件名：29_i2c_lcd_controller.py
-#  版本：V2.0
-#  author: zhulin
-#  说明：IIC lcd_controller液晶显示器模块实验
 #####################################################
-import lcd_controller   # lcd_controller液晶显示屏库
+import lcd_controller   # lcd_controller LCD display library
 import time
 from buzzer_controller import buzzer_controller
 from heartrate_monitor import HeartRateMonitor
 
-# 初始化lcd_controller液晶模块
+# initialize lcd_controller liquid crystal module
 def lcd_setup():
-	lcd_controller.makerobo_init(0x27, 1)	# 初始化(设备地址, 背光设置)
-	lcd_controller.makerobo_write(0, 0, 'sensor starting')     # 显示第一行信息
-	lcd_controller.makerobo_write(0, 1, 'made by Billy') # 显示第二行信息
+	lcd_controller.makerobo_init(0x27, 1)	# initialize device address and backlight setting
+	lcd_controller.makerobo_write(0, 0, 'sensor starting')     # display the first line of information
+	lcd_controller.makerobo_write(0, 1, 'made by Billy') # display the second line of information
 	time.sleep(4)
 
 def clear_screen():
-	lcd_controller.makerobo_init(0x27, 1)	# 初始化(设备地址, 背光设置)
+	lcd_controller.makerobo_init(0x27, 1)	# initialize device address and backlight setting
 	lcd_controller.makerobo_write(0, 0, ' '*15)
 
 def write_to_screen(text : str, line:int):
@@ -29,7 +24,7 @@ def write_to_screen(text : str, line:int):
 	lcd_controller.makerobo_write(0, line, text) 
 	
 
-# 循环函数
+# round function
 def main_loop(hrm:HeartRateMonitor):
 	while True:
 		time.sleep(1)
@@ -48,20 +43,20 @@ def main_loop(hrm:HeartRateMonitor):
 			buzzer_controller.playsong(2)
 			
 
-# 释放资源
+# release resources
 def destroy():
 	clear_screen()
 	hrm.stop_sensor()
 
-# 程序入口
+# program entry
 if __name__ == "__main__":
 	try:
 		hrm = HeartRateMonitor(False, True)
 		hrm.start_sensor()
-		# 开始了线程, 控制线程播放动画
-		lcd_setup()      # 初始化信息
+		# start the thread, control the thread to play the animation
+		lcd_setup()      # initialize information
 		clear_screen()
-		main_loop(hrm)       # 循环显示信息
+		main_loop(hrm)       # Cycle through information
 
-	except KeyboardInterrupt: # 当按下Ctrl+C时，将执行destroy()子程序。
-		destroy()             # 释放资源
+	except KeyboardInterrupt: # When Ctrl+C is pressed, the destroy() subroutine will be executed.
+		destroy()             # release resources
